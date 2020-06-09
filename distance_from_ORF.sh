97be8537fd3088dde49b2b0b815bc4787d8094bd
Peter Carlton <pmcarlton@mac.com>
2020-06-09 10:02:39 +0900

    Update distance_from_ORF.sh

--- distance_from_ORF.sh
+++ distance_from_ORF.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 PATTERN=${1? No search term given}
 DNAFILE=${2:-c_elegans_ws274.fna}
-CDSFILE=${3:-c_elegans_ws274_cds.gff3}
+GFFFILE=${3:-c_elegans_ws274_cds.gff3}
 
 fuzznuc -comp -pat $PATTERN -seq $DNAFILE -out temporary.txt
 < temporary.txt awk 'BEGIN{OFS="\t";} /pattern:/{print $1,$2,$3}' > matches.txt 
 < temporary.txt awk '/HitCount:/{print $3}' > count.txt 
 #rm ~/temporary.txt
     
-awk 'BEGIN{OFS="\t";} /\t\+\t/{print $1,$4,$7,$8,$9}' $CDSFILE > start-posi-ex.txt
-awk 'BEGIN{OFS="\t";} /\t\-\t/{print $1,$5,$7,$8,$9}' $CDSFILE > end-posi-ex.txt
+awk 'BEGIN{OFS="\t";} /\t\+\t/{print $1,$4,$7,$8,$9}' $GFFFILE > start-posi-ex.txt
+awk 'BEGIN{OFS="\t";} /\t\-\t/{print $1,$5,$7,$8,$9}' $GFFFILE > end-posi-ex.txt
 
 python redundancy-disposal.py start-posi-ex.txt end-posi-ex.txt
 #rm ~/genomicdata/start-posi-ex.txt ~/genomicdata/end-posi-ex.txt
